e2cdae752336ace2a66d813cf69f1d55e964cae1
rsalah <rsalah@gn4me.com>
2019-07-04 14:12:27 +0200

    update login service

--- app/src/main/java/com/example/akrar/model/UserService.java
+++ app/src/main/java/com/example/akrar/model/UserService.java
@@ -2,12 +2,14 @@ package com.example.akrar.model;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface UserService {
 
+    @FormUrlEncoded
     @POST("login")
     Call login(@Field("email") String username, @Field("password") String password);
 }
