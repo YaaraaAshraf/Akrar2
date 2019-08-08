68cb566812a82eef7e0323a0aa75cfa6c7d6a37c
Otex <yaaraashraff@gmail.com>
2019-08-08 12:44:06 +0200

    Shipment&Financial Invoice Layout

---
+++ app/src/main/java/com/example/akrar/Financial_Invoice.java
@@ -0,0 +1,13 @@
+package com.example.akrar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Financial_Invoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_financial__invoice);
+    }
+}
